91e4952af6bba4c77939fc3cdc4d96558f5c1dd6
edengebrezgi <edengebrezgi@gmail.com>
2021-04-28 19:54:33 -0400

    add file

---
+++ app/routes.js
@@ -0,0 +1,200 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+// const stripeSecret = process.env.STRIPE_SECRET_KEY
+// const stripePublic = process.env.STRIPE_PUBLISHABLE_KEY
+const stripe = require('stripe')('sk_test_51IiqpnFFrATjQHRASn48nRadpNh78B9tu0JYXZROA7ShR27z4SIJPn4pbJc8mlk9M2IP6AyyM9bmwnonAFev5JYH004v19wM4J'); // Add your Secret Key Here
+
+module.exports = function(app, passport, db) {
+
+// normal routes ===============================================================
+
+    // show the home page (will also have our login links)
+    app.get('/', function(req, res) {
+        res.render('index.ejs');
+    });
+
+    app.get('/payment', function(req, res) {
+        res.render('payment.ejs');
+    });
+
+
+
+
+    app.post("/charge", (req, res) => {
+      try {
+        stripe.customers
+          .create({
+            name: req.body.name,
+            email: req.body.email,
+            source: req.body.stripeToken
+          })
+          .then(customer =>
+            stripe.charges.create({
+              amount: req.body.amount * 100,
+              currency: "usd",
+              customer: customer.id
+            })
+          )
+          .then(() => res.render("completed.ejs"))
+          .catch(err => console.log(err));
+      } catch (err) {
+        res.send(err);
+      }
+    });
+
+    // PROFILE SECTION =========================
+    app.get('/profile', isLoggedIn, function(req, res) {
+        db.collection('messages').find().toArray((err, result) => {
+          if (err) return console.log(err)
+          res.render('profile.ejs', {
+            user : req.user,
+            messages: result
+          })
+        })
+    });
+
+    // MAKE ONE FOR EACH COUNTRY
+    app.get('/ethiopia', isLoggedIn, function(req, res) {
+      db.collection('ethiopia').find().toArray((err, result) => {
+        console.log(result)
+        if (err) return console.log(err)
+        res.render('ethiopia.ejs', {
+          ethiopia: result
+        })
+      })
+    });
+
+    app.get('/japan', isLoggedIn, function(req, res) {
+      console.log('japan')
+      db.collection('japan').trim().find().toArray((err, result) => {
+        console.log(result)
+        if (err) return console.log(err)
+        res.render('japan.ejs', {
+          japan: result
+        })
+      })
+    });
+
+
+  app.get('/price', isLoggedIn, function(req, res) {
+    db.collection('japan').find().toArray((err, result) => {
+      console.log('result',result);
+      if (err) return console.log(err)
+      res.render('price.ejs', {
+        test: result
+      })
+    })
+});
+
+    // LOGOUT ==============================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+// message board routes ===============================================================
+
+    app.post('/messages', (req, res) => {
+      db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+        if (err) return console.log(err)
+        console.log('saved to database')
+        res.redirect('/profile')
+      })
+    })
+
+    app.put('/messages', (req, res) => {
+      db.collection('messages')
+      .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+        $set: {
+          thumbUp:req.body.thumbUp + 1
+        }
+      }, {
+        sort: {_id: -1},
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+    })
+
+
+    app.put('/thumbDown', (req, res) => {
+      db.collection('messages')
+      .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+        $set: {
+          thumbDown:req.body.thumbDown + 1
+        }
+      }, {
+        sort: {_id: -1},
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+    })
+
+    app.delete('/messages', (req, res) => {
+      db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+        if (err) return res.send(500, err)
+        res.send('Message deleted!')
+      })
+    })
+
+// =============================================================================
+// AUTHENTICATE (FIRST LOGIN) ==================================================
+// =============================================================================
+
+    // locally --------------------------------
+        // LOGIN ===============================
+        // show the login form
+        app.get('/login', function(req, res) {
+            res.render('login.ejs', { message: req.flash('loginMessage') });
+        });
+
+        // process the login form
+        app.post('/login', passport.authenticate('local-login', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/login', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
+        // SIGNUP =================================
+        // show the signup form
+        app.get('/signup', function(req, res) {
+            res.render('signup.ejs', { message: req.flash('signupMessage') });
+        });
+
+        // process the signup form
+        app.post('/signup', passport.authenticate('local-signup', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/signup', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
+// =============================================================================
+// UNLINK ACCOUNTS =============================================================
+// =============================================================================
+// used to unlink accounts. for social accounts, just remove the token
+// for local account, remove email and password
+// user account will stay active in case they want to reconnect in the future
+
+    // local -----------------------------------
+    app.get('/unlink/local', isLoggedIn, function(req, res) {
+        var user            = req.user;
+        user.local.email    = undefined;
+        user.local.password = undefined;
+        user.save(function(err) {
+            res.redirect('/profile');
+        });
+    });
+
+};
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.redirect('/');
+}
